d26c571ab252140bfb35ad1d74f1b4fcaf883416
Dudelord <85459353+LorOfTheDudes@users.noreply.github.com>
2023-12-18 19:24:36 +0100

    Added dash to player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,65 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public int health = 3;
-
     public Rigidbody2D rb;
 
     Vector2 movement;
 
-    // Update is called once per frame
+    //Dash stuff
+    private float activeMoveSpeed;
+    public float dashSpeed;
+
+    public float dashLength = .5f, dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+
+    private void Start()
+    {
+        activeMoveSpeed = moveSpeed;
+    }
+
     void Update()
     {
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+
+        rb.velocity = movement * activeMoveSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space was pressed!");
+            if (dashCounter <=0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
         if( health <= 0)
         {
             Debug.Log("U ded!");
         }
     }
-
+    /*
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+    */
 }
